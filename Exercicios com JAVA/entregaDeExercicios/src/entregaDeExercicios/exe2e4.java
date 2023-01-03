ed2cb8b13a201b4003af2b53a1a773befa8ed7d7
unknown <oms.otavio@gmail.com>
2023-01-03 00:24:26 -0300

    Exe 2e4 certo

--- Exercicios com JAVA/entregaDeExercicios/src/entregaDeExercicios/exe2e4.java
+++ Exercicios com JAVA/entregaDeExercicios/src/entregaDeExercicios/exe2e4.java
@@ -3,7 +3,77 @@ package entregaDeExercicios;
 public class exe2e4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		Scanner leia = new Scanner(System.in);
+		float qtd;
+		int produto;
+		System.out.println("\n\t\tCARDÀPIO");
+		System.out.println("\n1 - Cachorro-quente = R$ 10.00");
+		System.out.println("2 - X-Salada = R$ 15.00 ");
+		System.out.println("3 - X-Bacon = R$ 18.00 ");
+		System.out.println("4 - Bauru = R$ 12.00 ");
+		System.out.println("5 - Refrigerante = R$ 8.00 ");
+		System.out.println("6 - Suco de laranja = R$ 13.00 ");
+		System.out.println("Qual opção voçê deseja: ");
+		produto = leia.nextInt();
+		System.out.println("Qual a quantidade voçê deseja: ");
+		qtd = leia.nextFloat();
+		
+		switch(produto) {
+		case 1:
+			System.out.println("Voçê escolheu: Cachorro-quente ");
+			System.out.println("Valor total: " + qtd * 10);
+			break;
+		case 2:
+			System.out.println("Voçê escolheu: X-Salada");
+			System.out.println("Valor total: " + qtd * 15);
+			break;
+		case 3:
+			System.out.println("Voçê escolheu: X-Bacon ");
+			System.out.println("Valor total: " + qtd * 18);
+			break;
+		case 4:
+			System.out.println("Voçê escolheu: Bauru");
+			System.out.println("Valor total: " + qtd * 12);
+			break;
+		case 5:
+			System.out.println("Voçê escolheu: Refrigerante");
+			System.out.println("Valor total: " + qtd * 8);
+			break;
+		case 6:
+			System.out.println("Voçê escolheu: Suco de Laranja");
+			System.out.println("Valor total: " + qtd * 13);
+			break;
+			default:
+				System.out.println("Opção Inválida");
+				*/
+		
+		//exercicio 4
+		
+		/*
+		 * Scanner leia = new Scanner(System.in);
+		float salario;
+		System.out.println("CALCÙLO DO IMPOSTO ! ");
+		System.out.println("Digite seu salário:  ");
+		salario = leia.nextFloat();
+		if(salario <= 2000.00) {
+			System.out.println("Voçê esta isento do IR");
+		}
+		else if(salario >= 2000.01 && salario <= 3000.00) {
+			System.out.println("IR: " + salario * 0.08);
+		}
+		else if(salario >= 3000.01 && salario <4.500) {
+			System.out.println("IR: " + salario * 0.18);
+		}
+		else if(salario > 4.500) {
+			System.out.println("IR: " + salario * 0.28);
+		}
+		 */
+		
+		}
+	}
+
+}
 
 	}
 
